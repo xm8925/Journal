9cd17aa45edc4534cb886c1fe24cafcaa0aa0171
xm8925 <innocence510@gmail.com>
2022-08-16 18:52:48 -0700

    added guest mode

---
+++ flask_app/static/js/script.js
@@ -0,0 +1,14 @@
+console.log("Welcome to my application")
+
+function edit() {
+    alert("This will lead you to a page where you can edit this form!");
+}
+
+function deleteThis(){
+    alert("This will delete the entry and send you back to the dashboard");
+}
+
+function image(){
+    alert("Clicking here will enable you to upload an image to the cloudinary api");
+}
+
